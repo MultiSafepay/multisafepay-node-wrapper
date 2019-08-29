d8e436943e4bd079921415fdee28fa57529ad3ed
Raúl Mena <raul.mena@multisafepay.com>
2019-08-29 15:01:04 +0200

    Fix example

--- examples/express/app.js
+++ examples/express/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const MSP = require('@multisafepay/api-wrapper-test').default;
+const MSP = require('@multisafepay/api-wrapper').default;
 const app = express();
 const port = 3000;
 
